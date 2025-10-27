e39f4ba5965ac3a3a25f91ef0ce6af5374afd2a1
Ankesh2004 <gankesh808@gmail.com>
2025-10-27 23:27:18 +0530

    feat: config handler

---
+++ config/config.go
@@ -0,0 +1,25 @@
+package config
+
+import "os"
+
+type Config struct {
+	MONGO_URI  string
+	DB_NAME    string
+	JWT_SECRET string
+	API_PORT   string
+}
+func LoadConfig() *Config {
+	return &Config{
+		MONGO_URI:  getEnv("MONGO_URI", "mongodb://localhost:27017"),
+		DB_NAME:    getEnv("DB_NAME", "presently"),
+		JWT_SECRET: getEnv("JWT_SECRET", "your_jwt_secret_key"),
+		API_PORT:   getEnv("API_PORT", "8080"),
+	}
+}
+func getEnv(key, fallback string) string {
+	val,err:= os.LookupEnv(key)
+	if err {
+		return fallback
+	}
+	return val
+}
