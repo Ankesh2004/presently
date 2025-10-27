674a0dd116f1b457bec36266b477f1c1f524a5a3
Ankesh2004 <gankesh808@gmail.com>
2025-10-27 19:03:58 +0530

    refactor:models

--- Makefile
+++ Makefile
@@ -22,7 +22,7 @@ run: build
 	./bin/starter-api 9999
 
 # Using reflex to watch for changes to .go file
-# and re-run `make run`
+# and re-run `make run` 
 # https://github.com/cespare/reflex/issues/50#issuecomment-388099690
 # Install with `go get github.com/cespare/reflex`
 # Make sure that GOPATH is setup. On OSX see:
--- api/models/attendance.go
+++ api/models/attendance.go
@@ -1,26 +1,10 @@
-package api
+package models
 
 import (
 	"time"
 
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
-
-type User struct {
-	ID       primitive.ObjectID `bson:"_id,omitempty" json:"id"`
-	Name     string             `bson:"name" json:"name"`
-	Email    string             `bson:"email" json:"email"`
-	Password string             `bson:"password" json:"-"`
-	Role     string             `bson:"role" json:"role"` // student or teacher
-}
-
-type Classroom struct {
-	ID           primitive.ObjectID   `bson:"_id,omitempty" json:"id"`
-	Name         string               `bson:"name" json:"name"`
-	InstructorID primitive.ObjectID   `bson:"instructorId" json:"instructorId"`
-	StudentIDs   []primitive.ObjectID `bson:"studentIds" json:"studentIds"`
-	UniqueCode   string               `bson:"uniqueCode" json:"uniqueCode"`
-}
 
 type AttendanceSession struct {
 	ID          primitive.ObjectID `bson:"_id,omitempty" json:"id"`
